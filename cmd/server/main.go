a8a4a6210bc123e9a195f440f67d9be582e0ed5a
Fabien Piette <fab.piette@gmail.com>
2026-03-06 10:43:45 +0100

    feat: project scaffold with chi router

---
+++ cmd/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	navidromeURL := mustEnv("NAVIDROME_URL")
+	navidromeUser := mustEnv("NAVIDROME_USER")
+	navidromePass := mustEnv("NAVIDROME_PASS")
+	port := envOr("PORT", "8080")
+
+	_ = navidromeURL
+	_ = navidromeUser
+	_ = navidromePass
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	log.Printf("listening on :%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func mustEnv(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		log.Fatalf("required env var %s is not set", key)
+	}
+	return v
+}
+
+func envOr(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/user/navidrome-playlists
+
+go 1.25.3
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
